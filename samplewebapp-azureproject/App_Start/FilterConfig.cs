97bc08818c2d2a838419fd4708db0f7c961760c4
Reventh Rao <reventhrao.anugu@lntinfotech.com>
2020-12-18 10:00:18 +0530

    azure web app

---
+++ samplewebapp-azureproject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace samplewebapp_azureproject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ samplewebapp-azureproject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="samplewebapp_azureproject.MvcApplication" Language="C#" %>
